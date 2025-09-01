3a5afd3c7b3cc8cb61c6d2e3b5bc35d1b1e9efb3
Sahil Madeshiya <sahilmadeshiya36@gmail.com>
2025-09-01 15:27:20 +0530

    add backend part

---
+++ backend/models/job.model.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const jobSchema = new mongoose.Schema({
+    title:{
+      type:String,
+      required:true,
+    },
+    description:{
+      type:String,
+      required:true,
+    },
+    requirements:{
+      type:[String],
+      required:true,
+    },
+    salary:{
+      type:String,
+      required:true,
+    },
+    experienceLevel:{
+      type: Number,
+      reqired: true, 
+    },
+    location:{
+      type: String,
+      require: true,
+    },
+    jobType:{
+      type: String,
+      require: true,
+    },
+    position:{
+      type: Number,
+      require: true,
+    },
+    company:{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Company',
+      require: true,
+    },
+    created_by:{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      require: true,
+    },
+    application:[
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Application',
+      }
+    ]
+},{timestamps: true});
+
+export const Job = mongoose.model("Job", jobSchema);
