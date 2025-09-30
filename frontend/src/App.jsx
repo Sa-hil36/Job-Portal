3089fa41fc9f68cf0275b06b95266713c119d57e
Sahil Madeshiya <sahilmadeshiya36@gmail.com>
2025-10-01 01:23:59 +0530

    created navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/shared/Navbar.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <h1 className= 'bg-red-500 text-green-200'>Let Us Build Job Portal</h1>
+      <Navbar/>
     </>
   )
 }
---
+++ frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import {Popover, PopoverContent, PopoverTrigger} from '../ui/popover'
+import {Avatar, AvatarFallback, AvatarImage} from '../ui/avatar'
+import {Button} from '../ui/button'
+import {User2, LogOut} from 'lucide-react'
+
+function Navbar() {
+  const user = true;
+  return (
+    <div className='bg-white'>
+      <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
+      <div>
+        <h1 className='text-2xl font-bold'>Job<span className='text-[#F83002]'>Portal</span></h1>
+      </div>
+      <div className='flex items-center gap-2'>
+        <ul className='flex font-medium items-center gap-5'>
+          <li>Home</li>
+          <li>Jobs</li>
+          <li>Browse</li>
+        </ul>
+        {
+          !user ? (
+            <div>
+              <Button variant='outline'>Login</Button>
+              <Button className='bg-[#6A38C2] hover:bg-[#5b30a6]'>Signup</Button>
+            </div>
+          ) : ( <Popover>
+          <PopoverTrigger>
+            <Avatar className="cursar-pointer">
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </PopoverTrigger>
+          <PopoverContent className="w-80">
+            <div className='flex gap-4 space-y-2'>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn"></AvatarImage>
+            </Avatar>
+            <div>
+              <h4 className='font-medium'> Sahil Madeshiya</h4>
+              <p className='text-sm text-muted-foreground'>Lorem ipsum dolor sit, amet</p>
+            </div>
+            </div>
+            <div className='flex flex-col text-gray-600'>
+              <div className='flex w-fit items-center gap-2 cursor-pointer'>
+                <User2/>
+              <Button variant="link">View Profile</Button>
+              </div>
+              <div className='flex w-fit items-center gap-2 cursor-pointer'>
+                <LogOut/>
+              <Button variant="link">Logout</Button>
+              </div>
+            </div>
+
+            </PopoverContent>
+        </Popover>
+        )
+      }
+      </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar
